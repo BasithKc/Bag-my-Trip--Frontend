e1c067050d8e390a3dbd8bb671e94a0ff2180efc
basith <abdulbasithkc313@gmail.com>
2024-09-24 15:42:20 +0530

    Created module user and home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren:()=> {
+    return import('./modules/user/user.module').then(m => m.UserModule);
+  }}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
