74fefdb4cfc4473faf47210a42e55c3979827c92
basith <abdulbasithkc313@gmail.com>
2024-09-30 11:57:20 +0530

    Resort booking section added in home component

--- src/app/modules/shared/animations.ts
+++ src/app/modules/shared/animations.ts
@@ -3,11 +3,11 @@ import { animate, state, style, transition, trigger } from "@angular/animations"
 export const slideInBottom = trigger("slideInBottom", [
     state("hidden", style({
         opacity : 0,
-        transform: 'translateY(100px)'
+        transform: 'translateY(100px); margin-right(1px)'
     })),
     state("visible", style({
         opacity : 1,
-        transform : 'translateY(0)'
+        transform : 'translateY(0) margin-right(1px)'
     })),
     transition("hidden => visible", [
         animate("1s ease-out")
--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { slideInBottom } from '../../animations';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations: [
+    slideInBottom
+  ]
 })
 export class FooterComponent {
-
 }
