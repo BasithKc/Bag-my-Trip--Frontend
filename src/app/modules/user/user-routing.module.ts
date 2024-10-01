e06a4f9781e3e85a1c63a2afac26c62799390388
basith <abdulbasithkc313@gmail.com>
2024-10-01 17:57:43 +0530

    package details component created

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
+import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
+import { PackageDetails } from "./packgeDetailsPage/packageDetails";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'package-details', component: PackageDetails}
 ]
 
 @NgModule({
