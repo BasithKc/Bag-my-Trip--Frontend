fa4efaf97745937ca2d010190a9449d9d52ef164
basith <abdulbasithkc313@gmail.com>
2024-10-01 10:34:27 +0530

    some bug fixed

--- src/app/modules/user/components/home/home.component.ts
+++ src/app/modules/user/components/home/home.component.ts
@@ -1,10 +1,29 @@
 import { Component, ViewChild } from '@angular/core';
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { ChangeDetectorRef } from "@angular/core";
+import { fadeIn, slideInBottom, slideInLeft } from "src/app/modules/shared/animations";
 import { TripPlanPopupComponent } from '../trip-plan-popup/trip-plan-popup.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations : [
+    fadeIn, slideInLeft, 
+    trigger("slideInBottom", [
+        state("hidden", style({
+            opacity : 0.4,
+            transform: 'translateY(35px) rotate(-90deg) scale(.97)'
+        })),
+        state("visible", style({
+            opacity : 1,
+            transform : '*'
+        })),
+        transition("hidden => visible", [
+            animate("1s 2s ease-out")
+        ])
+    ]),
+]
 })
 export class HomeComponent {
  @ViewChild(TripPlanPopupComponent) tripPlanPopup!: TripPlanPopupComponent
@@ -25,4 +44,18 @@ export class HomeComponent {
     const url = `https://wa.me/${this.phoneNumber}?text=${encodeURIComponent(this.message)}`;
     window.open(url, '_blank')
   }
+
+  constructor(private cdr: ChangeDetectorRef) {}
+    textAnimation: boolean = false
+    imgAnimation: boolean = false
+
+    onTextView(): void {
+        this.textAnimation = true
+        this.cdr.detectChanges()
+    }
+
+    onImgView(): void {
+        this.imgAnimation = true
+        this.cdr.detectChanges()
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,19 @@ module.exports = {
         'light_wide': '0px 6px 12px 0px rgba(0, 0, 0, 0.08), 0px 4px 8px 0px rgba(0, 0, 0, 0.04)'
       },
       backgroundImage: {
-        "honey_main" : "url(/assets/images/honeymoon/honey-main-cut.jpeg)",
-        "honey_1" : "url(/assets/images/honeymoon/honey-1.jpeg)",
-        "honey_2" : "url(/assets/images/honeymoon/honey-2.jpeg)",
-        "honey_3" : "url(/assets/images/honeymoon/honey-3.jpeg)",
-        "honey_4" : "url(/assets/images/honeymoon/honey-4.png)",
+        "honey_main": "url(/assets/images/honeymoon/honey-main-cut.jpeg)",
+        "honey_1": "url(/assets/images/honeymoon/honey-1.jpeg)",
+        "honey_2": "url(/assets/images/honeymoon/honey-2.jpeg)",
+        "honey_3": "url(/assets/images/honeymoon/honey-3.jpeg)",
+        "honey_4": "url(/assets/images/honeymoon/honey-4.png)",
+
+        "kashmir_main": "url(/assets/images/kashmir/kashmir -4.jpg)",
+        "kashmir_1": "url(/assets/images/kashmir/kashmir -1.jpg)",
+        "kashmir_2": "url(/assets/images/kashmir/kashmir -2.jpg)",
+        "kashmir_3": "url(/assets/images/kashmir/kashmir -3.jpg)",
       },
       screens: {
-        "xs" : "400px",
+        "xs": "400px",
       }
     },
   },
