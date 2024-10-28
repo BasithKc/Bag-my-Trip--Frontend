48bdc975846febd6d8831d53a7d7588091c58ab5
basith <abdulbasithkc313@gmail.com>
2024-10-28 17:44:00 +0530

    deploy to aws ec2

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -1,4 +1,4 @@
 export const Environment = {
-  // baseUrl: 'http://13.49.69.58:5000'
-  baseUrl: 'http://localhost:5000'
+  baseUrl: 'http://13.49.69.58:5000'
+  // baseUrl: 'http://localhost:5000'
 }
