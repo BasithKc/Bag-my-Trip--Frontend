8248b88432351e1b6c0fc67cf43b4830cc813a00
basith <abdulbasithkc313@gmail.com>
2024-11-01 11:42:08 +0530

    deploy to ec2

--- src/app/modules/user/package/components/package-card/packageCard.component.ts
+++ src/app/modules/user/package/components/package-card/packageCard.component.ts
@@ -7,15 +7,6 @@ import { TourService } from "../../../services/tour.service";
     styleUrls : ["./packageCard.component.css"]
 })
 export class PackageCardComponent implements OnInit{
-    packageList = [
-        { img : "/assets/images/package/pic-1.png", heading : "Switzweland", description : "Qui tempore voluptate qui quia commodi rem praesentium alias et.", date : "12 Sep ", strength : "120+ people", cost : "₹ 1,100", rating : "5" },
-        { img : "/assets/images/package/pic-2.png", heading : "Switzweland", description : "Qui tempore voluptate qui quia commodi rem praesentium alias et.", date : "12 Sep ", strength : "120+ people", cost : "₹ 1,100", rating : "5" },
-        { img : "/assets/images/package/pic-3.png", heading : "Switzweland", description : "Qui tempore voluptate qui quia commodi rem praesentium alias et.", date : "12 Sep ", strength : "120+ people", cost : "$ 1,100", rating : "5" },
-        { img : "/assets/images/package/pic-4.png", heading : "Switzweland", description : "Qui tempore voluptate qui quia commodi rem praesentium alias et.", date : "12 Sep ", strength : "120+ people", cost : "$ 1,100", rating : "5" },
-        { img : "/assets/images/package/pic-5.png", heading : "Switzweland", description : "Qui tempore voluptate qui quia commodi rem praesentium alias et.", date : "12 Sep ", strength : "120+ people", cost : "$ 1,100", rating : "5" },
-        { img : "/assets/images/package/pic-6.png", heading : "Switzweland", description : "Qui tempore voluptate qui quia commodi rem praesentium alias et.", date : "12 Sep ", strength : "120+ people", cost : "$ 1,100", rating : "5" },
-
-    ]
     tours: any [] = []
 
     constructor(private tourService: TourService) {}
